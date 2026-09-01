3136fe99154c9a7c55668777cf620dbff1e30579
yangbinji <45192687@qq.com>
2026-09-01 20:36:59 +0800

    使用spi机制，让client符合开闭原则

--- pickupcode/src/main/java/com/it/code/client/Client.java
+++ pickupcode/src/main/java/com/it/code/client/Client.java
@@ -1,24 +1,12 @@
 package com.it.code.client;
-
-
 import com.it.code.service.PickupCodeGenerator;
-import com.it.code.factory.DateCodeFactory;
-import com.it.code.factory.LayerCodeFactory;
 import com.it.code.factory.PickupCodeFactory;
-import com.it.code.factory.ShelfCodeFactory;
 import com.it.code.styl.Code;
 
-import java.awt.image.AreaAveragingScaleFilter;
 import java.util.*;
 
 public class Client {
-    private static Map<Integer, PickupCodeFactory>factoryMap;
-    static {
-        factoryMap=new HashMap<>();
-        factoryMap.put(1,new DateCodeFactory());
-        factoryMap.put(2,new ShelfCodeFactory());
-        factoryMap.put(3,new LayerCodeFactory());
-    }
+
 
     public static void main(String[] args) {
         List<PickupCodeFactory>list=new ArrayList<>();
@@ -33,24 +21,20 @@ public class Client {
         }
 
 
-
-
-
         Scanner scanner = new Scanner(System.in);
         int stylChoice=scanner.nextInt();
-        PickupCodeFactory factory = factoryMap.get(stylChoice);
+        PickupCodeFactory factory = list.get(stylChoice-1);
 
         System.out.println("请选择生成取货码的方式");
         System.out.println("选项1 单步获取");
         System.out.println("选项2 一次性获取");
         int wayChoice=scanner.nextInt();
+        PickupCodeGenerator codeGenerator = new PickupCodeGenerator(factory);
         if(wayChoice==1){
-            PickupCodeGenerator codeGenerator = new PickupCodeGenerator(factory);
             while (!scanner.nextLine().equals("q")){
                 System.out.println(codeGenerator.getCodeStepByStep().getPickupCode());
             }
         }else if(wayChoice==2){
-            PickupCodeGenerator codeGenerator = new PickupCodeGenerator(factory);
             System.out.println("输入生成取货码的数量");
             List<Code> codeAtOnce = codeGenerator.getCodeAtOnce(scanner.nextInt());
             for(Code code:codeAtOnce){
