f1fb04c33eae8cecb57f68a45b3cf399fea62ce3
yangbinji <45192687@qq.com>
2026-09-06 11:17:47 +0800

    给ParamSpec类增加type类型

--- pickupcode/src/main/java/com/it/code/parameter/ParamSpec.java
+++ pickupcode/src/main/java/com/it/code/parameter/ParamSpec.java
@@ -1,19 +1,27 @@
 package com.it.code.parameter;
 
+import com.it.code.enums.ParamType;
+
 public class ParamSpec {
     private String name; //参数名称
     private String desc; //参数说明
+    private ParamType type;//参数类型
 
-    public ParamSpec(String name, String desc) {
+    public ParamSpec(String name, String desc,ParamType type) {
         this.name = name;
         this.desc = desc;
+        this.type=type;
     }
 
     public String getName() {
         return name;
+
     }
 
     public String getDesc() {
         return desc;
+    }
+    public ParamType getType(){
+        return type;
     }
 }
